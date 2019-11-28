2de6cfbdd3de9b9aabf1a6515c8323fbeb1a7dd4
kovgreen <kovgreen@gmail.com>
2019-11-28 13:33:09 +0200

    Fixed#3 HW-01

--- goit-js-hw-01/js/task-1_5.js
+++ goit-js-hw-01/js/task-1_5.js
@@ -1,25 +1,29 @@
 'use strict';
-let userChoiceCountry = prompt('Укажите страну доставки');
+const userChoiceCountry = prompt('Укажите страну доставки');
+const priceChina = 100;
+const priceChile = 250;
+const priceAustralia = 170;
+const priceIndia = 80;
+const priceJamaica = 120;
 
 if (userChoiceCountry === null) {
   console.log('До встречи!');
 } else {
-  userChoiceCountry = userChoiceCountry.toLowerCase();
-  switch (userChoiceCountry) {
+  switch (userChoiceCountry.toLowerCase()) {
     case 'китай':
-      alert(`Доставка в Китай будет стоить 100 кредитов`);
+      alert(`Доставка в Китай будет стоить ${priceChina} кредитов`);
       break;
     case 'чили':
-      alert(`Доставка в Чили будет стоить 250 кредитов`);
+      alert(`Доставка в Чили будет стоить ${priceChile} кредитов`);
       break;
     case 'австралия':
-      alert(`Доставка в Австралию будет стоить 170 кредитов`);
+      alert(`Доставка в Австралию будет стоить ${priceAustralia} кредитов`);
       break;
     case 'индия':
-      alert(`Доставка в Китай будет стоить 80 кредитов`);
+      alert(`Доставка в Китай будет стоить ${priceIndia} кредитов`);
       break;
     case 'ямайка':
-      alert(`Доставка в Ямайку будет стоить 120 кредитов`);
+      alert(`Доставка в Ямайку будет стоить ${priceJamaica} кредитов`);
       break;
     default:
       alert(`В вашей стране доставка не доступна`);
--- goit-js-hw-01/js/task-1_6.js
+++ goit-js-hw-01/js/task-1_6.js
@@ -6,13 +6,11 @@ let total = 0;
 do {
   input = prompt(`Введите любое число. Для получения результата нажмите "ESC"`);
   let inputNumber = Number(input);
-  if (isNaN(inputNumber)) {
+  if (Number.isNaN(inputNumber)) {
     alert(`Было введено не число, попробуйте еще раз`);
-    inputNumber = 0;
+    continue;
   }
   total += inputNumber;
 } while (input !== null);
 
-if (total > 0 && total !== NaN) {
-  alert(`Общая сумма чисел равна ${total}`);
-}
+total > 0 ? alert(`Общая сумма чисел равна ${total}`) : '';
